22e31f5c807405b120e9b8e5c1c543eb9ca894d6
liufangri <liufangri_sxy@163.com>
2015-05-27 22:46:38 +0800

    可以刷评论自娱自乐了

--- app/src/main/java/com/example/team/myapplication/LoginState.java
+++ app/src/main/java/com/example/team/myapplication/LoginState.java
@@ -4,6 +4,8 @@ package com.example.team.myapplication;
 public class LoginState {
     public static boolean logined = false;
     public static String username="guest";
+    public static String userEmail = "null";
+    public static int userID = 0;
     public LoginState(){
     }
     public static void setLogined(boolean _logined,String username){
--- app/src/main/java/com/example/team/myapplication/ViewPictureActivity.java
+++ app/src/main/java/com/example/team/myapplication/ViewPictureActivity.java
@@ -3,6 +3,7 @@ package com.example.team.myapplication;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +25,7 @@ public class ViewPictureActivity extends GeneralActivity {
     private LinearLayout commentView;
     private ProgressBar progressBar;
     private List<Comment> comments;
+    private View scrollView;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +40,8 @@ public class ViewPictureActivity extends GeneralActivity {
         editText = (EditText)findViewById(R.id.comment_text);
         commentView = (LinearLayout)findViewById(R.id.comment_view);
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        comments = new ArrayList<>();//评论的ArrayList 数组，把获得的
-
+        comments = new ArrayList<>();//评论的ArrayList 数组，把获得的评论放在这里
+        scrollView = findViewById(R.id.scrollView);
         getComments();
 
 
@@ -77,49 +79,69 @@ public class ViewPictureActivity extends GeneralActivity {
 
     //在此上传评论
     public void submitComment(View view){
-        String comment = editText.getText().toString();
+        String comment = editText.getText().toString();//获取输入的评论
+        if(comment.isEmpty()){
 
+            return;
+        }
+        addComment(comment);//在用户界面能看到评论更新
+        editText.setText(null);
+    }
+
+    public void addComment(String comment){
+        comments.add(new Comment(getApplicationContext(), LoginState.username + ": ", comment));
+        commentView.addView(comments.get(comments.size()-1));
+        Thread refresh = new Thread(new RefreshThread());
+        refresh.start();
+        //接下来把评论传到数据库
 
     }
 
     public void getComments(){
+        //获取评论函数
+
+
         //测试显示评论.
         comments.add(new Comment(getApplicationContext(),
                 "sxy" + ": " , "评论在这里（5毛一条，括号里不要复制）"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy"+": ","23333"));
-        comments.add(new Comment(getApplicationContext(), "sxy" + ": ", "23333"));
+
+
         for(int i = 0;i<comments.size();i++){
             commentView.addView(comments.get(i));
         }
     }
-    class myThread implements Runnable{
 
 
-        @Override
+    class RefreshThread implements Runnable {
+
         public void run() {
-            try{
-                Thread.sleep(3000);
-                comments.add(new Comment(getApplicationContext(), "sxy" + ": ", "23333"));
-                commentView.postInvalidate();
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                try {
+
+                    Thread.sleep(100);
+
+                }
+
+                catch (InterruptedException e) {
+
+                    Thread.currentThread().interrupt();
+
+                }
+                // 使用postInvalidate可以直接在线程中更新界面
+
+                scrollView.postInvalidate();
+                Log.d("Comment------------>","Refreshing!!");
+                Thread.currentThread().interrupt();
 
             }
-            catch (InterruptedException e){
-                //
-            }
+
         }
+
     }
+
+
+
 
 }
