644cd01896131c7fb4ef48c2df45ace8e1deb53b
cocowhr <whr4545@yeah.net>
2015-05-27 17:01:45 +0900

    加入评论提交函数

--- app/src/main/java/com/example/team/myapplication/ViewPictureActivity.java
+++ app/src/main/java/com/example/team/myapplication/ViewPictureActivity.java
@@ -9,12 +9,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.team.myapplication.Cache.Localstorage;
+import com.example.team.myapplication.Database.DB;
+import com.example.team.myapplication.Network.AES;
+import com.example.team.myapplication.Network.JsonPost;
 import com.example.team.myapplication.util.GeneralActivity;
+
+import java.util.HashMap;
 
 
 public class ViewPictureActivity extends GeneralActivity {
     ImageView imgview;
     private EditText editText;
+    private DB db = new DB(this);
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +74,14 @@ public class ViewPictureActivity extends GeneralActivity {
     //在此上传评论
     public void submitComment(){
         String comment = editText.getText().toString();
-
+        String key = "1234567891234567";
+        String username= LoginState.username;
+        AES aesEncrypt = new AES(key);
+        String encryptUsername=aesEncrypt.encrypt(username);
+        String url = "http://192.168.137.1/php2/index.php";
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("username", encryptUsername);
+        map.put("content", comment);
+        JsonPost post = new JsonPost(map, url,3,db);
     }
 }
